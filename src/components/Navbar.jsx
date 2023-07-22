08517fdacfb49930edaa0e72ee495c0107466832
shubhamnaik2303 <108280093+shubhamnaik2303@users.noreply.github.com>
2023-07-22 22:45:59 +0530

    gg

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,7 +2,7 @@ import React, {useEffect, useState} from 'react'
 import { Link } from "react-router-dom"
 import { styles } from "../styles"
 import { navLinks } from '../constants'
-import {logo, menu, close} from "../assets"
+import {logoo, menu, close} from "../assets"
 
 const Navbar = () => {
   const[active, setActive] = useState("")
@@ -19,7 +19,7 @@ const Navbar = () => {
             setActive("");
             window.scrollTo (0,0);
         }}>
-          <img src={logo} alt='logo'
+          <img src={logoo} alt='logo'
           className='w-9 h-9 object-contain'/>
           <p className='text-white text-[18px] flex font-bold cursor-pointer'>
           Shubham &nbsp;
